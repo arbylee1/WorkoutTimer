d4ab6d2fee07484783a621d32c2abbe3bcad8c50
Albert Li <arbylee@gmail.com>
2017-07-11 23:48:12 -0400

    first commit

---
+++ app/src/main/java/biz/albertli/workouttimer/model/Workout.java
@@ -0,0 +1,15 @@
+package biz.albertli.workouttimer.model;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import biz.albertli.workouttimer.R;
+
+public class Workout {
+}
